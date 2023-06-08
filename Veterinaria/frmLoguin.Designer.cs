62352d14c5f247b90cd7ad89534d6b83ae6907cf
Christopher Mendoza <aguileracris57@gmail.com>
2023-06-08 01:07:26 -0600

    Subiendo Loguin

---
+++ Veterinaria/frmLoguin.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Veterinaria
+{
+    partial class frmLoguin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLoguin));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.btnLoguin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(98, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(98, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(98, 157);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(303, 23);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtContra
+            // 
+            this.txtContra.Location = new System.Drawing.Point(98, 255);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.PasswordChar = '*';
+            this.txtContra.Size = new System.Drawing.Size(303, 23);
+            this.txtContra.TabIndex = 3;
+            // 
+            // btnLoguin
+            // 
+            this.btnLoguin.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLoguin.FlatAppearance.BorderSize = 0;
+            this.btnLoguin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoguin.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLoguin.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnLoguin.Location = new System.Drawing.Point(98, 319);
+            this.btnLoguin.Name = "btnLoguin";
+            this.btnLoguin.Size = new System.Drawing.Size(303, 36);
+            this.btnLoguin.TabIndex = 4;
+            this.btnLoguin.Text = "Iniciar Sesion";
+            this.btnLoguin.UseVisualStyleBackColor = false;
+            this.btnLoguin.Click += new System.EventHandler(this.btnLoguin_Click);
+            // 
+            // frmLoguin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(498, 481);
+            this.Controls.Add(this.btnLoguin);
+            this.Controls.Add(this.txtContra);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmLoguin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loguin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsuario;
+        private TextBox txtContra;
+        private Button btnLoguin;
+    }
+}
---
+++ Veterinaria/frmLoguin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veterinaria
+{
+    public partial class frmLoguin : Form
+    {
+        public frmLoguin()
+        {
+            InitializeComponent();
+        }
+        int a = 0;
+
+        private void btnLoguin_Click(object sender, EventArgs e)
+        {
+            string User = "VAAdmin";
+            string Pass = "Admin*2023";
+            txtContra.Text.Equals(Pass);
+            txtUsuario.Text.Equals(User);
+            Form1 obj = new Form1();
+
+            if (txtUsuario.Text.Equals(User) && txtContra.Text.Equals(Pass))
+            {
+                obj.ShowDialog();
+                txtUsuario.Clear();
+                txtContra.Clear();
+
+            }
+            else
+            {
+                a = a + 1;
+                MessageBox.Show("Datos no encontrado, solo puede intentar 3 veces, lleva " + a + " intentos");
+                txtContra.Clear();
+                if (a == 3)
+                {
+                    MessageBox.Show("Error, no se han encontrado sus datos, saldra del programa");
+                    a = 0;
+                    Application.Exit();
+
+                }
+            }
+
+        }
+    }
+}
